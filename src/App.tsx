9172b1bfa2cb9204c4473b1907302458341e1fbd
Ante Penić <antepenic94@gmail.com>
2022-03-03 10:47:45 +0100

    Revert "Update App.tsx"
    This reverts commit 55b7ddfa6f1585c744eb0761ced9270512afb576.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import Board from "./components/Board";
 
 function App() {
-    var moves = 1;
-
     return (
         <main>
             <section>
                 <h1>Memory Card Game</h1>
                 <div>
-                    <p>Moves: 0</p>
+                    <p>Moves</p>
                 </div>
             </section>
             <section>
