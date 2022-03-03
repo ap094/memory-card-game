55b7ddfa6f1585c744eb0761ced9270512afb576
Ante Penić <antepenic94@gmail.com>
2022-03-03 10:47:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Board from "./components/Board";
 
 function App() {
+    var moves = 1;
+
     return (
         <main>
             <section>
                 <h1>Memory Card Game</h1>
                 <div>
-                    <p>Moves</p>
+                    <p>Moves: 0</p>
                 </div>
             </section>
             <section>
