6b8f3f1625e4aa431416cc31c32d3557222200b0
Ante Penić <antepenic94@gmail.com>
2022-03-03 10:38:20 +0100

    Create GameContext.tsx

---
+++ src/context/GameContext.tsx
@@ -0,0 +1,15 @@
+import { createContext, FC } from "react";
+
+interface IGameContext {}
+
+export const GameContext = createContext<IGameContext>({} as IGameContext);
+
+export const GameProvider: FC = ({ children }) => {
+    const value: IGameContext = {};
+
+    return (
+        <GameContext.Provider value={value}>
+            {children}
+        </GameContext.Provider>
+    );
+};
