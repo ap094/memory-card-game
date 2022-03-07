b8857466c4d41ae48db7319e43b1be8ca6a1d396
Ante Penić <antepenic94@gmail.com>
2022-03-07 23:59:24 +0100

    config updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslintPlugin from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
 
 export default defineConfig({
 	plugins: [react(), eslintPlugin(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			"@scss": path.resolve(__dirname, "./src/assets/scss"),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+                    @import '@scss/01-settings/_breakpoints.scss';
+                    @import '@scss/01-settings/_colors.scss';
+                    @import '@scss/01-settings/_fonts.scss';
+                    @import '@scss/02-tools/_functions.scss';
+                `,
+			},
+		},
+	},
 });
