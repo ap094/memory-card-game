e68e053840d95b64b00d80b906d99d4369a94231
Ante Penić <antepenic94@gmail.com>
2022-03-09 15:16:37 +0100

    eslint import config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,14 @@ module.exports = {
 		ecmaVersion: "latest",
 		sourceType: "module",
 	},
-	plugins: ["react", "@typescript-eslint"],
+	plugins: ["react", "@typescript-eslint", "import"],
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:react/recommended",
 		"plugin:react-hooks/recommended",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
 		"prettier",
 	],
 	rules: {
@@ -28,10 +30,50 @@ module.exports = {
 		"react/react-in-jsx-scope": "off",
 		"@typescript-eslint/no-empty-interface": "warn",
 		"@typescript-eslint/no-unused-vars": "warn",
+		"@typescript-eslint/no-empty-function": "warn",
+        "sort-imports": [
+            "warn",
+            {
+                "ignoreCase": true,
+                "ignoreDeclarationSort": true,
+            },
+        ],
+		"import/order": [
+			"warn",
+			{
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    "parent",
+                    "sibling",
+                    "index",
+                ],
+				pathGroups: [
+					{
+                        pattern: "react",
+                        group: "builtin",
+                        position: "before",
+					},
+				],
+				pathGroupsExcludedImportTypes: ["react"],
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+			},
+		],
+        "import/no-unresolved": "error",
 	},
 	settings: {
 		react: {
 			version: "detect",
+		},
+        "import/resolver": {
+            node: {
+                paths: ["src"],
+                extensions: [".ts", ".tsx"],
+            },
 		},
 	},
 };
