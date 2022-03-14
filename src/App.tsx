9e9fdb0992b3ac9b78c6a1f375f6c701d3e5cb6d
Ante Penić <antepenic94@gmail.com>
2022-03-14 12:57:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { Actions } from "components/Actions";
 import { Board } from "components/Board";
-import { Results } from "components/Results";
 
 function App() {
 	return (
 		<main>
 			<h1>Memory Card Game</h1>
-			<Actions />
 			<Board />
-			<Results />
 		</main>
 	);
 }
