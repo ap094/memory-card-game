2ebd46f8d463a705231075ee88eeebff932404c0
Ante Penić <antepenic94@gmail.com>
2022-03-15 10:34:20 +0100

    github workflow setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import * as path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
-import * as path from "path";
 
 export default defineConfig({
 	plugins: [react(), eslintPlugin(), tsconfigPaths()],
